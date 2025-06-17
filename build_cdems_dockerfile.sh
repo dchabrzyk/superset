95109bd29178c4b8ae61f836003af485cf9d6a68
Marcin Kozaczyk <kozaczyk.marcin@gmail.com>
2025-06-17 14:00:25 +0200

    chore: version 5.0.0rc3-masta-2

--- build_cdems_dockerfile.sh
+++ build_cdems_dockerfile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --tag masta.azurecr.io/superset:4.1.1-masta-1 --platform=linux/amd64 --push -f ./CDEMS.Dockerfile .
+docker buildx build --tag masta.azurecr.io/superset:5.0.0-rc3-masta-2 --platform=linux/amd64 --push -f ./CDEMS.Dockerfile .
