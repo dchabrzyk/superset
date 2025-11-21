faf5592c740537528e2d64337d3f623b8fe6f3be
Dobiesław <d.chabrzyk@gmail.com>
2025-11-21 23:17:51 +0100

    fix(docker): add missing build argument for translations in build script

--- build_cdems_dockerfile.sh
+++ build_cdems_dockerfile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --tag masta.azurecr.io/superset:6.0.0rc3-masta-1 --platform=linux/amd64 --push -f ./CDEMS.Dockerfile .
+docker buildx build --build-arg BUILD_TRANSLATIONS=true --tag masta.azurecr.io/superset:6.0.0rc3-masta-1 --platform=linux/amd64 --push -f ./CDEMS.Dockerfile .
