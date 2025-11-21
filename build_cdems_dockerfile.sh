b8e54d788071084ca2106bf511400a6c93f31450
Dobiesław <d.chabrzyk@gmail.com>
2025-11-21 22:28:24 +0100

    fix(docker): update build command to use buildx and correct image tag

--- build_cdems_dockerfile.sh
+++ build_cdems_dockerfile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t masta.azurecr.io/superset:6.0.0rc3-masta-1 -f ./CDEMS.Dockerfile .
+docker buildx build --tag masta.azurecr.io/superset:superset:6.0.0rc3-masta-1 --platform=linux/amd64 --push -f ./CDEMS.Dockerfile .
